1b90d486c9668ffade3873d757e1e74ac153e563
Fernando Rodrigues <lkgfernando@gmail.com>
2022-06-22 21:51:00 -0300

    Update

---
+++ JAVASCRIPT/js/objetos.js
@@ -0,0 +1,22 @@
+
+
+//Objetos
+
+let obj = document.getElementById('resultado')
+
+let pessoa = { 
+  nome: "Fernando José Rodrigues",
+  idade: 37,
+  altura: 1.75,
+  cargo: "Desenvolvedor"
+}
+
+console.log(pessoa)
+
+
+let usuario = [
+  {nome: 'Fernando', cargo: 'Programador', status: 'ATIVO'},
+  {nome: 'José', cargo: 'Backend', status: 'INATIVO'}
+]
+
+console.log(usuario)
---
+++ JAVASCRIPT/js/templeteString.js
@@ -0,0 +1,11 @@
+
+let resultado = document.getElementById('visualizar')
+
+
+let nome = "Fernando"
+let sobrenome = "Rodrigues"
+let idade = 37
+
+let mensagem = `Meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos de idade`
+
+resultado.innerHTML = mensagem
