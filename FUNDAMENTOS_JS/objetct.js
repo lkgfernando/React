08e4638d17cefafb9dcc7eea6ac738fe82463938
Fernando José Rodrigues <66326365+lkgfernando@users.noreply.github.com>
2022-07-26 12:22:13 -0300

    Update

--- FUNDAMENTOS_JS/objetct.js
+++ FUNDAMENTOS_JS/objetct.js
@@ -5,3 +5,11 @@ prod1.preco = 4998.90
 prod1['Desconto Legal'] = 0.40
 
 console.log(prod1)
+
+const prod2 = {
+    nome: "Teclado Microsoft",
+    valor: 115.20
+}
+
+prod2['Unidade'] = "UN"
+console.log(prod2)
