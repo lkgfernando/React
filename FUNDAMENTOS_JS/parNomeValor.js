458df4a763981dca042a5b1a4adc651d5f9d0f7c
Fernando José Rodrigues <66326365+lkgfernando@users.noreply.github.com>
2022-08-04 13:33:02 -0300

    Uptade

---
+++ FUNDAMENTOS_JS/parNomeValor.js
@@ -0,0 +1,21 @@
+const saudacao = "Opa"
+
+function exec(){
+    const saudacao = "Fala"
+    return saudacao
+}
+
+const clientes = {
+    nome: "Fernando",
+    idade: 37,
+    peso: 108,
+    edereco: {
+        rua: "Rua Javascript fundamentos, 61",
+        bairro: "Programação",
+        cidade: "A mais próxima possível"
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(clientes)
