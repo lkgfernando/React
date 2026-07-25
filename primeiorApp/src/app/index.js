14889c3cb7e1446cae81a1351a79fd505f90c33c
Fernando Rodrigues <lkgfernando@gmail.com>
2026-07-24 21:25:52 -0300

    fix: Aulas de alinhamento de flexbox, recebendo dados, botoes, textInput

--- primeiorApp/src/app/index.js
+++ primeiorApp/src/app/index.js
@@ -1,24 +1,60 @@
 import React, { Component } from "react";
-import { Image, Text, View } from "react-native";
+import { StyleSheet, TextInput, View, Text, Button } from "react-native";
 
 class App extends Component {
-  render() {
-    let nome = "Fernando rodrigues";
-    let img = "https://sujeitoprogramador.com/steve.png";
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: "",
+      input: "",
+    };
 
+    this.entrar = this.entrar.bind(this);
+  }
+
+  entrar() {
+    if (this.state.input === "") {
+      alert("Digite seu nome!");
+      return;
+    }
+    this.setState({ nome: "Bem vindo: " + this.state.input });
+  }
+
+  render() {
     return (
       <View>
-        <Text>Helo Worde!</Text>
-        <Text>Meu primeiro App!</Text>
-        <Text style={{ color: "#ff0000", fontSize: 25, margin: 15 }}>
-          Sujeito Programador!
-        </Text>
-        <Image source={{ uri: img }} style={{ width: 300, height: 300 }} />
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu nome?"
+          underlineColorAndroid="transparent"
+          onChangeText={(texto) => this.setState({ input: texto })}
+        />
 
-        <Text style={{ fontSize: 30 }}> {nome} </Text>
+        <Button title="Entrar" onPress={this.entrar} />
+
+        <Text style={styles.texto}>{this.state.nome}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  input: {
+    height: 45,
+    borderWidth: 1,
+    borderColor: "#222",
+    margin: 10,
+    fontSize: 20,
+    padding: 10,
+    marginTop: 40,
+  },
+  texto: {
+    textAlign: "center",
+    fontSize: 25,
+  },
+});
 
 export default App;
